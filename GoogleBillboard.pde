8afc630b2c105d1e15f83bf53727d782f4df23ca
unknown <jalin@697-334-Rushey.sf.k12.internal>
2016-01-07 10:30:35 -0800

    Finished

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,17 +1,15 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
-int found=0;
 
 public void setup()  
 {            
-     noLoop();  
-     for(int i=0; found<1; i++){
-     	double tenDigit= Double.parseDouble(e.substring(i+2,i+12));
+    noLoop();  
+    for(int i=2; i<e.length(); i++){
+    double tenDigit= Double.parseDouble(e.substring(i,i+10));
      	if(isPrime(tenDigit)==true){
-     		found++;
      		System.out.println(tenDigit);
-
-     }
-     	}
+            break;
+        }
+    }
 }  
 public void draw()  
 {   
@@ -24,9 +22,9 @@ public boolean isPrime(double dNum)
     for(int n=2; n<=Math.sqrt(dNum); n++){
 		if(dNum%n==0)
 		factors++;
-		}
-		if(factors==0)
-		return true;
-		else
-		return false;
+	}
+	if(factors==0)
+	return true;
+	else
+	return false;
 } 
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -15,19 +15,17 @@ import java.io.IOException;
 public class GoogleBillboard extends PApplet {
 
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
-int found=0;
 
 public void setup()  
 {            
-     noLoop();  
-     for(int i=0; found<1; i++){
-     	double tenDigit= Double.parseDouble(e.substring(i+2,i+12));
+    noLoop();  
+    for(int i=2; i<e.length(); i++){
+    double tenDigit= Double.parseDouble(e.substring(i,i+10));
      	if(isPrime(tenDigit)==true){
-     		found++;
      		System.out.println(tenDigit);
-
-     }
-     	}
+            break;
+        }
+    }
 }  
 public void draw()  
 {   
@@ -40,11 +38,11 @@ public boolean isPrime(double dNum)
     for(int n=2; n<=Math.sqrt(dNum); n++){
 		if(dNum%n==0)
 		factors++;
-		}
-		if(factors==0)
-		return true;
-		else
-		return false;
+	}
+	if(factors==0)
+	return true;
+	else
+	return false;
 } 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
